40f768d1d8a5b9b46cf3834becf4316982f82589
Auraaf <12791azeezfarooqui@gmail.com>
2024-02-04 08:25:02 +0530

    E8P3 Updating State variablein class based components

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -6,16 +6,31 @@ class UserClass extends React.Component{
         super(props) ;
         this.state ={
             count : 0 ,
+            count1 : 1 
         } ;
     }
     render(){
         const {name} = this.props;
         //object destucturing of state variable.
-        const {count} = this.state;
+        const {count , count1} = this.state;
        return (
         <div>
             <div className="user-card">
+                <button onClick={() => {
+
+                        this.setState({
+                            count : this.state.count + 1
+                        })
+                }}   > Update count</button>
+                 <button onClick={() => {
+                    this.setState({
+                        count : this.state.count + 1 ,
+                        count1 : this.state.count1 + 1 ,
+                    });
+                 }}> Update count and count1</button>
                 <h5>count : {count}</h5>
+                <h5>count1 : {count1}</h5>
+
                 <h3>Name : {name}</h3>
                 <h4>Sde-1</h4>
             </div>
